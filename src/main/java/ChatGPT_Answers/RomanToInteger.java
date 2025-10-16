7bc22f3cdb6c66c238fd71273f34847de16c588a
rodriguezaj2 <rodriguezaj2020@gmail.com>
2025-10-15 21:16:27 -0400

    Roman To Integer Leetcode problem 13

--- src/main/java/ChatGPT_Answers/RomanToInteger.java
+++ src/main/java/ChatGPT_Answers/RomanToInteger.java
@@ -3,43 +3,43 @@ package ChatGPT_Answers;
 import java.util.HashMap;
 import java.util.Map;
 
-    public class RomanToInteger {
-        public static int romanToInt(String s) {
-            Map<Character, Integer> map = new HashMap<>();
-            map.put('I', 1);
-            map.put('V', 5);
-            map.put('X', 10);
-            map.put('L', 50);
-            map.put('C', 100);
-            map.put('D', 500);
-            map.put('M', 1000);
+public class RomanToInteger {
+    public static int romanToInt(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
 
-            int total=0;
+        int total = 0;
 
-            for (int i = 0; i < s.length(); i++) {
-                int current=map.get(s.charAt(i));
+        for (int i = 0; i < s.length(); i++) {
+            int current = map.get(s.charAt(i));
 
-                if(i+1<s.length()){
-                    int next = map.get(s.charAt(i+1));
+            if (i + 1 < s.length()) {
+                int next = map.get(s.charAt(i + 1));
 
-                    if(current<next){
-                        total -=current;
-                    }else {
-                        total += current;
-                    }
-                    }else{
-                        total +=current;
-                    }
+                if (current < next) {
+                    total -= current;
+                } else {
+                    total += current;
                 }
-                return total;
+            } else {
+                total += current;
             }
+        }
+        return total;
+    }
 
-        public static void main(String[] args) {
-            System.out.println(romanToInt("III")); //3
-            System.out.println(romanToInt("LVIII")); //58
-            System.out.println(romanToInt("MCMXCIV")); //1994
-        }
-        }
+    public static void main(String[] args) {
+        System.out.println(romanToInt("III")); //3
+        System.out.println(romanToInt("LVIII")); //58
+        System.out.println(romanToInt("MCMXCIV")); //1994
+    }
+}
 
 
 
